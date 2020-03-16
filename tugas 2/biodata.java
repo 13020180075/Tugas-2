808805b3016fb155571a9dfddff27d7a653c7a5b
13020180075 <61999761+13020180075@users.noreply.github.com>
2020-03-16 22:26:40 +0800

    Add files via upload

---
+++ tugas 2/biodata.java
@@ -0,0 +1,28 @@
+/*
+	nama : ilfah sahra
+	tgl : 16 maret 2020
+	waktu : 22.00
+*/
+
+public class biodata {
+	public static void main (String [] args){
+
+	String Nama = "ilfah sahra";
+	String Nim = "13020180075";
+	String jurusan = "teknik informatika";
+	String fakultas = " ilmu komputer";
+	
+
+	System.out.println("Nama = "+Nama);
+	System.out.println("Nim   =" +Nim);
+	System.out.println("Jurusan  ="+jurusan);
+	System.out.println("Fakultas ="+fakultas);
+	
+
+
+}
+
+
+}
+
+	
